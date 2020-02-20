25d499d2909aa90c41e666bb42a473a4f9be0526
Rafal-Weglinski <52180356+Rafal-Weglinski@users.noreply.github.com>
2020-02-20 11:17:03 +0100

    Starting commit

---
+++ src/homepage.component.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className='directory-menu'>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">dasd</h1>
+                    <span className="subtitle">
+dasdfsdsa
+                    </span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">dadasdsd</h1>
+                    <span className="subtitle">
+dasdsghja
+                    </span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">dasdasdsadd</h1>
+                    <span className="subtitle">
+da,mn,sdsa
+                    </span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">dasfsdfdfd</h1>
+                    <span className="subtitle">
+dasds,jh,a
+                    </span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">dasfsdfd</h1>
+                    <span className="subtitle">
+d,mn,asdsa
+                    </span>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+)
+
+export default HomePage;
